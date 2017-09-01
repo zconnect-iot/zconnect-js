a2aca98c2de73b42ee04850a9074a9de3b512f32
Jon Wyatt <jon@zoetrope.io>
2017-09-01 16:38:00 +0100

    Switch action types to use core instead of app

--- api/constants.js
+++ api/constants.js
@@ -1,12 +1,12 @@
-export const REQUEST = 'app/api/REQUEST'
-export const REQUEST_PENDING = 'app/api/REQUEST_PENDING'
-export const REQUEST_ERROR = 'app/api/REQUEST_ERROR'
-export const REQUEST_SUCCESS = 'app/api/REQUEST_SUCCESS'
-export const REQUEST_CACHE_USED = 'app/api/REQUEST_CACHE_USED'
+export const REQUEST = 'core/api/REQUEST'
+export const REQUEST_PENDING = 'core/api/REQUEST_PENDING'
+export const REQUEST_ERROR = 'core/api/REQUEST_ERROR'
+export const REQUEST_SUCCESS = 'core/api/REQUEST_SUCCESS'
+export const REQUEST_CACHE_USED = 'core/api/REQUEST_CACHE_USED'
 
-export const POLL_REQUEST = 'app/api/POLL_REQUEST'
-export const STOP_POLL_REQUEST = 'app/api/STOP_POLL_REQUEST'
+export const POLL_REQUEST = 'core/api/POLL_REQUEST'
+export const STOP_POLL_REQUEST = 'core/api/STOP_POLL_REQUEST'
 
 export const FETCH_TIMEOUT = 10000
 
-export const REFRESH_JWT = 'app/auth/REFRESH_JWT'
+export const REFRESH_JWT = 'core/auth/REFRESH_JWT'
